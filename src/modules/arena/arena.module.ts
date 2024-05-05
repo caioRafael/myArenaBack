65333cae57a56d1011e2fe9a5ad09575d421c635
caioRafael <caiorafaelrg@gmail.com>
2024-05-05 16:00:23 -0300

    refatorando login e arena

--- src/modules/arena/arena.module.ts
+++ src/modules/arena/arena.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { ArenaService } from './arena.service';
 import { ArenaController } from './arena.controller';
 import { PrismaService } from '../../infra/database/prisma.service';
+import { IArenaRepository } from './repositories/arena.repository';
+import ArenaPrismaRepository from './repositories/prisma/arena.prisma.repository';
 
 @Module({
   imports: [],
   controllers: [ArenaController],
-  providers: [ArenaService, PrismaService],
+  providers: [
+    ArenaService,
+    PrismaService,
+    {
+      provide: IArenaRepository,
+      useClass: ArenaPrismaRepository,
+    },
+  ],
 })
 export class ArenaModule {}
--- src/modules/arena/arena.service.ts
+++ src/modules/arena/arena.service.ts
@@ -1,20 +1,17 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import ArenaDto from './dto/arena.dto';
+import ArenaDto, { UserDto } from './dto/arena.dto';
 import { PrismaService } from '../../infra/database/prisma.service';
 import { hash } from 'bcrypt';
+import { IArenaRepository } from './repositories/arena.repository';
 
 @Injectable()
 export class ArenaService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private arenaReository: IArenaRepository) {}
   async create(createArenaDto: ArenaDto) {
-    const user = await this.prisma.user.findFirst({
-      where: {
-        OR: [
-          { email: createArenaDto.administrator.email },
-          { phone: createArenaDto.administrator.phone },
-        ],
-      },
-    });
+    const user = await this.arenaReository.findUserByEmailOrPhone(
+      createArenaDto.administrator.email,
+      createArenaDto.administrator.phone,
+    );
 
     if (user && user.email === createArenaDto.administrator.email)
       throw new HttpException(
@@ -28,15 +25,12 @@ export class ArenaService {
         HttpStatus.BAD_REQUEST,
       );
 
-    const arena = await this.prisma.arena.findFirst({
-      where: {
-        OR: [
-          { cnpj: createArenaDto.cnpj },
-          { phone: createArenaDto.phone },
-          { corporateName: createArenaDto.corporateName },
-        ],
-      },
-    });
+    const arena =
+      await this.arenaReository.findArenaByCnpjOrPhoneOrCorporateName(
+        createArenaDto.cnpj,
+        createArenaDto.phone,
+        createArenaDto.corporateName,
+      );
 
     if (arena && arena.cnpj === createArenaDto.cnpj)
       throw new HttpException(
@@ -58,96 +52,61 @@ export class ArenaService {
 
     const password = await hash(createArenaDto.administrator.password, 10);
 
-    const newArena = await this.prisma.arena.create({
-      data: {
-        address: createArenaDto.address,
-        fantasyName: createArenaDto.fantasyName,
-        phone: createArenaDto.phone,
-        corporateName: createArenaDto.corporateName,
-        cnpj: createArenaDto?.cnpj,
-        pixKey: createArenaDto?.pixKey,
-        requirePrePayment: createArenaDto.requirePrePayment,
-        cep: createArenaDto.cep,
-        city: createArenaDto.city,
-        locale: createArenaDto.locale,
-        uf: createArenaDto.uf,
-      },
-    });
+    const arenaData = {
+      address: createArenaDto.address,
+      fantasyName: createArenaDto.fantasyName,
+      phone: createArenaDto.phone,
+      corporateName: createArenaDto.corporateName,
+      cnpj: createArenaDto?.cnpj,
+      pixKey: createArenaDto?.pixKey,
+      requirePrePayment: createArenaDto.requirePrePayment,
+      cep: createArenaDto.cep,
+      city: createArenaDto.city,
+      locale: createArenaDto.locale,
+      uf: createArenaDto.uf,
+    };
+    const newArena = await this.arenaReository.createArena(
+      arenaData as ArenaDto,
+    );
 
-    const newUser = await this.prisma.user.create({
-      data: {
-        email: createArenaDto.administrator.email,
-        name: createArenaDto.administrator.name,
-        nickname: createArenaDto.administrator.nickname,
-        password: password,
-        profile: 'ADMINISTRATOR',
-        arenaId: newArena.id,
-        phone: createArenaDto.administrator.phone,
-      },
-    });
+    const administratorData = {
+      email: createArenaDto.administrator.email,
+      name: createArenaDto.administrator.name,
+      nickname: createArenaDto.administrator.nickname,
+      password: password,
+      profile: 'ADMINISTRATOR',
+      arenaId: newArena.id,
+      phone: createArenaDto.administrator.phone,
+    };
+
+    const newUser = await this.arenaReository.createAdministrator(
+      administratorData as UserDto,
+    );
 
     if (newArena && newUser) return newArena;
   }
 
   async findAll() {
-    const arenaList = await this.prisma.arena.findMany();
+    const arenaList = await this.arenaReository.findAll();
 
     return arenaList;
   }
 
   async findByUser(userId: string) {
-    const arena = await this.prisma.arena.findFirst({
-      where: {
-        employees: {
-          some: {
-            id: userId,
-          },
-        },
-      },
-      include: {
-        employees: true,
-      },
-    });
+    const arena = await this.arenaReository.findByUser(userId);
 
     return arena;
   }
 
   async findOne(id: string) {
-    const arena = await this.prisma.arena.findUnique({
-      where: {
-        id: id,
-      },
-    });
+    const arena = await this.arenaReository.findOne(id);
 
     return arena;
   }
 
   async monthReport(id: string) {
-    const dataAtual = new Date();
-    const mesAtual = dataAtual.getMonth() + 1; // +1 porque os meses em JavaScript vão de 0 a 11
-    const anoAtual = dataAtual.getFullYear();
+    const arenaReport = await this.arenaReository.monthReport(id);
 
-    const arenaReport = await this.prisma.$queryRaw`
-      SELECT 
-        count(*) as schedules, 
-        SUM(fields.price) as espec,
-        SUM(CASE 
-            WHEN shcedule_time.status IN ('APPROVED', 'STARTED', 'FINISHED')
-            THEN fields.price / 2 
-            WHEN shcedule_time.status IN ('FINAL_PAYMENT', 'CLOSED')
-            THEN fields.price
-            ELSE 0 END) as revenue
-      FROM fields 
-        LEFT JOIN shcedule_time on fields.id = shcedule_time."fieldId"
-        WHERE fields."arenaId" = ${id}
-        AND shcedule_time.date BETWEEN ${new Date(
-          `${anoAtual}-${mesAtual}-01`,
-        )} AND ${new Date(
-      `${mesAtual + 1 === 13 ? anoAtual + 1 : anoAtual}-${
-        mesAtual + 1 === 13 ? 1 : mesAtual + 1
-      }-01`,
-    )}`;
-
-    return arenaReport[0];
+    return arenaReport;
   }
 }
